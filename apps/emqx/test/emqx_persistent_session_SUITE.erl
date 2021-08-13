9bef2e396ea7b5f75e438a5e7e6a41cb96ecadfa
Tobias Lindahl <tobias@happihacking.com>
2021-08-13 10:01:22 +0200

    test: Refactor and extend testing of persistent sessions

--- apps/emqx/test/emqx_persistent_session_SUITE.erl
+++ apps/emqx/test/emqx_persistent_session_SUITE.erl
@@ -27,19 +27,39 @@
 %%--------------------------------------------------------------------
 
 all() ->
-    [ {group, tcp}
-    , {group, quic}
+    [ {group, kill_connection_process}
+    , {group, no_kill_connection_process}
     ].
+
+%% A persistent session can be resumed in two ways:
+%%    1. The old connection process is still alive, and the session is taken
+%%       over by the new connection.
+%%    2. The old session process has died (e.g., because of node down).
+%%       The new process resumes the session from the stored state, and finds
+%%       any subscribed messages from the persistent message store.
+%%
+%% We want to test both these implementations, which is done through the top
+%% level groups {no_}kill_connection_process.
+%%
+%% In addition, we test both tcp and quic connections for both scenarios.
 
 groups() ->
     TCs = emqx_ct:all(?MODULE),
-    [ {tcp,  [], TCs}
+    [ {no_kill_connection_process, [], [{group, tcp}, {group, quic}]}
+    , {   kill_connection_process, [], [{group, tcp}, {group, quic}]}
+    , {tcp,  [], TCs}
     , {quic, [], TCs}
     ].
 
 init_per_group(tcp, Config) ->
     emqx_ct_helpers:start_apps([]),
-    [ {port, 1883}, {conn_fun, connect} | Config];
+    [ {port, 1883}, {conn_fun, connect},  {kill_connection_process, true}| Config];
+init_per_group(no_kill_connection_process, Config) ->
+    emqx_ct_helpers:start_apps([]),
+    [ {kill_connection_process, false} | Config];
+init_per_group(kill_connection_process, Config) ->
+    emqx_ct_helpers:start_apps([]),
+    [ {kill_connection_process, true} | Config];
 init_per_group(quic, Config) ->
     emqx_ct_helpers:start_apps([]),
     [ {port, 14567}, {conn_fun, quic_connect} | Config];
@@ -100,6 +120,29 @@ receive_messages(Count, Msgs) ->
         Msgs
     end.
 
+maybe_kill_connection_process(ClientId, Config) ->
+    case ?config(kill_connection_process, Config) of
+        true ->
+            [ConnectionPid] = emqx_cm:lookup_channels(ClientId),
+            ?assert(is_pid(ConnectionPid)),
+            exit(ConnectionPid, kill),
+            ok;
+        false ->
+            ok
+    end.
+
+publish(Topic, Payloads = [_|_], Config)->
+    ConnFun = ?config(conn_fun, Config),
+    {ok, Client} = emqtt:start_link([ {proto_ver, v5}
+                                    | Config]),
+    {ok, _} = emqtt:ConnFun(Client),
+    lists:foreach(fun(Payload) ->
+                          {ok, _} = emqtt:publish(Client, Topic, Payload, 2)
+                  end, Payloads),
+    ok = emqtt:disconnect(Client);
+publish(Topic, Payload, Config) ->
+    publish(Topic, [Payload], Config).
+
 %%--------------------------------------------------------------------
 %% Test Cases
 %%--------------------------------------------------------------------
@@ -108,9 +151,10 @@ receive_messages(Count, Msgs) ->
 t_connect_session_expiry_interval(Config) ->
     ConnFun = ?config(conn_fun, Config),
     Topic = <<"t_connect_session_expiry_interval/foo">>,
-    Payload = "test message",
+    Payload = <<"test message">>,
+    ClientId = <<"t_connect_session_expiry_interval">>,
 
-    {ok, Client1} = emqtt:start_link([ {clientid, <<"t_connect_session_expiry_interval">>},
+    {ok, Client1} = emqtt:start_link([ {clientid, ClientId},
                                        {proto_ver, v5},
                                        {properties, #{'Session-Expiry-Interval' => 7200}}
                                      | Config]),
@@ -118,24 +162,21 @@ t_connect_session_expiry_interval(Config) ->
     {ok, _, [2]} = emqtt:subscribe(Client1, Topic, qos2),
     ok = emqtt:disconnect(Client1),
 
-    {ok, Client2} = emqtt:start_link([{proto_ver, v5}
-                                     | Config]),
+    maybe_kill_connection_process(ClientId, Config),
 
-    {ok, _} = emqtt:ConnFun(Client2),
-    {ok, 2} = emqtt:publish(Client2, Topic, Payload, 2),
-    ok = emqtt:disconnect(Client2),
+    publish(Topic, Payload, Config),
 
-    {ok, Client3} = emqtt:start_link([ {clientid, <<"t_connect_session_expiry_interval">>},
+    {ok, Client2} = emqtt:start_link([ {clientid, ClientId},
                                        {proto_ver, v5},
                                        {properties, #{'Session-Expiry-Interval' => 7200}},
                                        {clean_start, false}
                                      | Config]),
-    {ok, _} = emqtt:ConnFun(Client3),
+    {ok, _} = emqtt:ConnFun(Client2),
     [Msg | _ ] = receive_messages(1),
     ?assertEqual({ok, iolist_to_binary(Topic)}, maps:find(topic, Msg)),
     ?assertEqual({ok, iolist_to_binary(Payload)}, maps:find(payload, Msg)),
     ?assertEqual({ok, 2}, maps:find(qos, Msg)),
-    ok = emqtt:disconnect(Client3).
+    ok = emqtt:disconnect(Client2).
 
 t_without_client_id(Config) ->
     process_flag(trap_exit, true), %% Emqtt client dies
@@ -158,6 +199,8 @@ t_assigned_clientid_persistent_session(Config) ->
     AssignedClientId = client_info(clientid, Client1),
     ok = emqtt:disconnect(Client1),
 
+    maybe_kill_connection_process(AssignedClientId, Config),
+
     {ok, Client2} = emqtt:start_link([ {clientid, AssignedClientId},
                                        {proto_ver, v5},
                                        {clean_start, false}
@@ -167,8 +210,11 @@ t_assigned_clientid_persistent_session(Config) ->
     ok = emqtt:disconnect(Client2).
 
 t_cancel_on_disconnect(Config) ->
+    %% Open a persistent session, but cancel the persistence when
+    %% shutting down the connection.
     ConnFun = ?config(conn_fun, Config),
     ClientId = <<"t_cancel_on_disconnect">>,
+
     {ok, Client1} = emqtt:start_link([ {proto_ver, v5},
                                        {clientid, ClientId},
                                        {properties, #{'Session-Expiry-Interval' => 16#FFFFFFFF}},
@@ -179,37 +225,12 @@ t_cancel_on_disconnect(Config) ->
 
     {ok, Client2} = emqtt:start_link([ {clientid, ClientId},
                                        {proto_ver, v5},
-                                       {clean_start, false}
+                                       {clean_start, false},
+                                       {properties, #{'Session-Expiry-Interval' => 16#FFFFFFFF}}
                                      | Config]),
     {ok, _} = emqtt:ConnFun(Client2),
     ?assertEqual(0, client_info(session_present, Client2)),
     ok = emqtt:disconnect(Client2).
-
-t_process_dies(Config) ->
-    %% Emulate an error in the connect process,
-    %% or that the node of the process goes down.
-    %% A persistent session should survive anyway.
-    ConnFun = ?config(conn_fun, Config),
-    ClientId = <<"t_process_dies">>,
-    {ok, Client1} = emqtt:start_link([ {proto_ver, v5},
-                                       {clientid, ClientId},
-                                       {properties, #{'Session-Expiry-Interval' => 16#FFFFFFFF}},
-                                       {clean_start, true}
-                                     | Config]),
-    {ok, _} = emqtt:ConnFun(Client1),
-    ok = emqtt:disconnect(Client1),
-
-    [ChannelPid] = emqx_cm:lookup_channels(ClientId),
-    ?assert(is_pid(ChannelPid)),
-    exit(ChannelPid, kill),
-
-    {ok, Client2} = emqtt:start_link([ {proto_ver, v5},
-                                       {clientid, ClientId},
-                                       {clean_start, false}
-                                     | Config]),
-    {ok, _} = emqtt:ConnFun(Client2),
-    ?assertEqual(1, client_info(session_present, Client2)),
-    emqtt:disconnect(Client2).
 
 t_process_dies_session_expires(Config) ->
     %% Emulate an error in the connect process,
@@ -225,9 +246,7 @@ t_process_dies_session_expires(Config) ->
     {ok, _} = emqtt:ConnFun(Client1),
     ok = emqtt:disconnect(Client1),
 
-    [ChannelPid] = emqx_cm:lookup_channels(ClientId),
-    ?assert(is_pid(ChannelPid)),
-    exit(ChannelPid, kill),
+    maybe_kill_connection_process(ClientId, Config),
 
     timer:sleep(1000),
 
@@ -239,71 +258,34 @@ t_process_dies_session_expires(Config) ->
     ?assertEqual(0, client_info(session_present, Client2)),
     emqtt:disconnect(Client2).
 
-t_clean_start_drops_subscriptions(Config) ->
+t_publish_while_client_is_gone(Config) ->
+    %% A persistent session should receive messages in its
+    %% subscription even if the process owning the session dies.
     ConnFun = ?config(conn_fun, Config),
-    Topic = <<"t_clean_start_drops_subscriptions/bar">>,
-    STopic = <<"t_clean_start_drops_subscriptions/+">>,
-    Payload = <<"hello">>,
-    ClientId = <<"t_clean_start_drops_subscriptions">>,
-    {ok, Client1} = emqtt:start_link([ {proto_ver, v5},
-                                       {clientid, ClientId},
-                                       {properties, #{'Session-Expiry-Interval' => 30}},
-                                       {clean_start, true}
-                                     | Config]),
-    {ok, _} = emqtt:ConnFun(Client1),
-    {ok, _, [0]} = emqtt:subscribe(Client1, STopic, 0),
-    ok = emqtt:disconnect(Client1),
-
-    {ok, Client2} = emqtt:start_link([ {proto_ver, v5},
-                                       {clientid, ClientId},
-                                       {clean_start, true}
-                                     | Config]),
-    {ok, _} = emqtt:ConnFun(Client2),
-    ?assertEqual(0, client_info(session_present, Client2)),
-    {ok, _, [0]} = emqtt:subscribe(Client2, STopic, 0),
-
-    {ok, Client3} = emqtt:start_link([{proto_ver, v5}
-                                     | Config]),
-    {ok, _} = emqtt:ConnFun(Client3),
-    {ok, 2} = emqtt:publish(Client3, Topic, Payload, 2),
-    ok = emqtt:disconnect(Client3),
-
-    [_Msg1] = receive_messages(1),
-
-    ok = emqtt:disconnect(Client2).
-
-t_subscription_while_process_is_gone(Config) ->
-    ConnFun = ?config(conn_fun, Config),
-    Topic = <<"t_clean_start_drops_subscriptions/bar">>,
-    STopic = <<"t_clean_start_drops_subscriptions/+">>,
+    Topic = <<"t_publish_while_client_is_gone/bar">>,
+    STopic = <<"t_publish_while_client_is_gone/+">>,
     Payload1 = <<"hello1">>,
     Payload2 = <<"hello2">>,
-    ClientId = <<"t_clean_start_drops_subscriptions">>,
+    ClientId = <<"t_publish_while_client_is_gone">>,
     {ok, Client1} = emqtt:start_link([ {proto_ver, v5},
                                        {clientid, ClientId},
                                        {properties, #{'Session-Expiry-Interval' => 30}},
                                        {clean_start, true}
                                      | Config]),
     {ok, _} = emqtt:ConnFun(Client1),
-    {ok, _, [2]} = emqtt:subscribe(Client1, STopic, 2),
+    {ok, _, [2]} = emqtt:subscribe(Client1, STopic, qos2),
+
     ok = emqtt:disconnect(Client1),
+    maybe_kill_connection_process(ClientId, Config),
 
-    [ChannelPid] = emqx_cm:lookup_channels(ClientId),
-    ?assert(is_pid(ChannelPid)),
-    exit(ChannelPid, kill),
+    ok = publish(Topic, [Payload1, Payload2], Config),
 
-    {ok, Client2} = emqtt:start_link([ {proto_ver, v5}
-                                     | Config]),
-    {ok, _} = emqtt:ConnFun(Client2),
-    {ok, 2} = emqtt:publish(Client2, Topic, Payload1, 2),
-    {ok, 3} = emqtt:publish(Client2, Topic, Payload2, 2),
-
-    {ok, Client3} = emqtt:start_link([ {proto_ver, v5},
+    {ok, Client2} = emqtt:start_link([ {proto_ver, v5},
                                        {clientid, ClientId},
                                        {properties, #{'Session-Expiry-Interval' => 30}},
                                        {clean_start, false}
                                      | Config]),
-    {ok, _} = emqtt:ConnFun(Client3),
+    {ok, _} = emqtt:ConnFun(Client2),
     [Msg1] = receive_messages(1),
     [Msg2] = receive_messages(1),
     ?assertEqual({ok, iolist_to_binary(Payload1)}, maps:find(payload, Msg1)),
@@ -311,6 +293,66 @@ t_subscription_while_process_is_gone(Config) ->
     ?assertEqual({ok, iolist_to_binary(Payload2)}, maps:find(payload, Msg2)),
     ?assertEqual({ok, 2}, maps:find(qos, Msg2)),
 
-    ok = emqtt:disconnect(Client2),
-    ok = emqtt:disconnect(Client3).
+    ok = emqtt:disconnect(Client2).
 
+t_clean_start_drops_subscriptions(Config) ->
+    %% 1. A persistent session is started and disconnected.
+    %% 2. While disconnected, a message is published and persisted.
+    %% 3. When connecting again, the clean start flag is set, the subscription is renewed,
+    %%    then we disconnect again.
+    %% 4. Finally, a new connection is made with clean start set to false.
+    %% The original message should not be delivered.
+
+    ConnFun = ?config(conn_fun, Config),
+    Topic = <<"t_clean_start_drops_subscriptions/bar">>,
+    STopic = <<"t_clean_start_drops_subscriptions/+">>,
+    Payload1 = <<"hello1">>,
+    Payload2 = <<"hello2">>,
+    Payload3 = <<"hello3">>,
+    ClientId = <<"t_clean_start_drops_subscriptions">>,
+
+    %% 1.
+    {ok, Client1} = emqtt:start_link([ {proto_ver, v5},
+                                       {clientid, ClientId},
+                                       {properties, #{'Session-Expiry-Interval' => 30}},
+                                       {clean_start, true}
+                                     | Config]),
+    {ok, _} = emqtt:ConnFun(Client1),
+    {ok, _, [2]} = emqtt:subscribe(Client1, STopic, qos2),
+
+    ok = emqtt:disconnect(Client1),
+    maybe_kill_connection_process(ClientId, Config),
+
+    %% 2.
+    ok = publish(Topic, Payload1, Config),
+
+    %% 3.
+    {ok, Client2} = emqtt:start_link([ {proto_ver, v5},
+                                       {clientid, ClientId},
+                                       {properties, #{'Session-Expiry-Interval' => 30}},
+                                       {clean_start, true}
+                                     | Config]),
+    {ok, _} = emqtt:ConnFun(Client2),
+    ?assertEqual(0, client_info(session_present, Client2)),
+    {ok, _, [2]} = emqtt:subscribe(Client2, STopic, qos2),
+
+    ok = publish(Topic, Payload2, Config),
+    [Msg1] = receive_messages(1),
+    ?assertEqual({ok, iolist_to_binary(Payload2)}, maps:find(payload, Msg1)),
+
+    ok = emqtt:disconnect(Client2),
+    maybe_kill_connection_process(ClientId, Config),
+
+    %% 4.
+    {ok, Client3} = emqtt:start_link([ {proto_ver, v5},
+                                       {clientid, ClientId},
+                                       {properties, #{'Session-Expiry-Interval' => 30}},
+                                       {clean_start, false}
+                                     | Config]),
+    {ok, _} = emqtt:ConnFun(Client3),
+
+    ok = publish(Topic, Payload3, Config),
+    [Msg2] = receive_messages(1),
+    ?assertEqual({ok, iolist_to_binary(Payload3)}, maps:find(payload, Msg2)),
+
+    ok = emqtt:disconnect(Client3).
